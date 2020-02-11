a19928ef08cf7c40dda34266dd97b79a5975d7b5
Alexander Mamani <akeymy4@gmail.com>
2020-02-11 19:17:30 -0400

    install artyom.js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import Artyom from 'artyom.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recetas-ihc';
+  jarvis = new Artyom();
+
+  constructor(){
+    this.jarvis.say("Hello World !");
+  }
+
 }
