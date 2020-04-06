f34c0f651095187050ecbf7b9a7f00490ef6b47c
root <draykoes@gmail.com>
2020-04-06 19:11:24 -0400

    models

---
+++ src/app/models/pasos.ts
@@ -0,0 +1,5 @@
+export class pasos {
+    Descripcion: string;
+    imagen: string;
+    numero: number;
+}
---
+++ src/app/models/receta.ts
@@ -0,0 +1,8 @@
+import { pasos } from './pasos';
+
+export class recetaDrayko {
+    id: string;
+    nombre: string;
+    ingredientes: Array<string>;
+    pasos: Array<pasos>;
+}
